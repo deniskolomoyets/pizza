00b0c5c8a72acbcac674b7c313d75623001f565f
Denis <kolomoetc@gmail.com>
2023-08-17 22:20:30 +0300

    #14 -deboubce input, -pagination redux toolkit

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,29 +1,46 @@
 import React from 'react';
+import { SearchContext } from '../../App';
+import debounce from 'lodash.debounce';
+
 import searchIcon from '../../assets/img/search-icon.svg';
 import cancelIcon from '../../assets/img/cancel-icon.svg';
+
 import styles from './Search.module.scss';
-import { SearchContext } from '../../App';
 
 const Search = () => {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const [value, setValue] = React.useState('');
+  const { setSearchValue } = React.useContext(SearchContext);
   const inputRef = React.useRef();
 
   const onClickClear = () => {
     setSearchValue('');
+    setValue('');
     inputRef.current.focus();
   }; //инпут с самонаведением с помощью хука useref
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 1000),
+    [],
+  ); //-если меняется value, то вызываем это действие; ссылка на функцию и сделали её отложенной
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
 
   return (
     <div className={styles.root}>
       <img className={styles.icon} src={searchIcon} alt="searchIcon" />
       <input
         ref={inputRef}
-        value={searchValue} //хранит то что мы меняем
-        onChange={(event) => setSearchValue(event.target.value)}
+        value={value} //хранит то что мы меняем
+        onChange={onChangeInput}
         className={styles.input}
         placeholder="Search pizzas"
       />
-      {searchValue && (
+      {value && (
         <img
           onClick={onClickClear}
           className={styles.cancelIcon}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { setCategoryId } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
@@ -14,15 +14,18 @@ const Home = () => {
   const dispatch = useDispatch();
   const categoryId = useSelector((state) => state.filter.categoryId);
   const sortType = useSelector((state) => state.filter.sort.sortProperty);
-
+  const currentPage = useSelector((state) => state.filter.currentPage);
 
   const { searchValue } = React.useContext(SearchContext); //контекст из апп.джсх
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-  const [currentPage, setCurrentPage] = React.useState(1);
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
+  };
+
+  const onChangePage = (number) => {
+    dispatch(setCurrentPage(number));
   };
 
   React.useEffect(() => {
@@ -45,8 +48,8 @@ const Home = () => {
       .get(
         `https://64aaeb3e0c6d844abedefbc9.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
       )
-      .then(res => {
-        setItems(res.data);//answer from backend is stored in date
+      .then((res) => {
+        setItems(res.data); //answer from backend is stored in date
         setIsLoading(false);
       });
     window.scrollTo(0, 0); //перекидывает на верх страницы при перезагрузке
@@ -71,8 +74,7 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
-      <Pagination onChangePage={(number) => setCurrentPage(number)} />{' '}
-      {/*onChangePage возвращает только число,а не обьект */}
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
 };
