c4df13a770dad370d271c64e09596b0d0eb4f669
Denis Kolomoyets <deniskolomoyets@Deniss-MacBook-Air.local>
2023-09-27 21:07:28 +0300

    #21 typescript

---
+++ src/pages/FullPizza.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const FullPizza: React.FC = () => {
+  const [pizza, setPizza] = React.useState<{
+    imageUrl: string;
+    title: string;
+    price: string;
+  }>();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get('https://64aaeb3e0c6d844abedefbc9.mockapi.io/items/' + id);
+        setPizza(data);
+      } catch (error) {
+        
+        alert('Error');
+        navigate('/')
+      }
+    }
+    fetchPizza();
+  }, []) ;
+  if (!pizza) {
+    return <>Downloading...</>;
+  }
+  return (
+    <div className="container">
+      <img src={pizza.imageUrl} alt="Pizza" />
+      <h2>{pizza.title}</h2>
+      <h4>{pizza.price}$</h4>
+    </div>
+  );
+};
+
+export default FullPizza;
